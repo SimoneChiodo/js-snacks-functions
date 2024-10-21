532274e7c95fce1afc3462c7b6fe7723695827e9
SimoneChiodo <simochi.005@gmail.com>
2024-10-21 16:35:00 +0200

    esercizio 3, 4

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,44 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+function countVowels(string) {
+    let vowelsCount = 0;
+    let vowelsArray = ["a", "e", "i", "o", "u"];
+    let vowelsFound = [];
 
+    // Per ogni lettera della stringa
+    for (let i = 0; i < string.length; i++) {
+        // Controllo ogni vocale
+        for (let y = 0; y < vowelsArray.length; y++) {
+            if (string[i] === vowelsArray[y]) {
+                vowelsCount++;
+                vowelsFound.push(string[i]);
+            }
+        }
+    }
+
+    return vowelsCount + ` (${vowelsFound.join(", ")})`;
+}
+
+const arrowCountVowels = (string) => {
+    let vowelsCount = 0;
+    const vowelsArray = ["a", "e", "i", "o", "u"];
+    let vowelsFound = [];
+
+    string.split("").forEach((char) => {
+        if (vowelsArray.includes(char)) {
+            vowelsCount++;
+            vowelsFound.push(char);
+        }
+    });
+
+    return vowelsCount + ` (${vowelsFound.join(", ")})`;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(countVowels(word));
+console.log(arrowCountVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+function hello(name) {
+    return `Ciao ${name}`;
+}
 
+const arrowHello = (name) => {
+    return `Ciao ${name}`;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(hello(name));
+console.log(arrowHello(name));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
