8798247f0a8892b45cf850fe48e2895f2c8231fa
SimoneChiodo <simochi.005@gmail.com>
2024-10-21 15:58:51 +0200

    esercizio 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,23 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function firstLettersArray(array) {
+    let result = [];
 
+    for (let i = 0; i < array.length; i++) {
+        result.push(array[i].substring(0, 1));
+    }
+
+    return result;
+}
+
+const arrowFirstLettersArray = (array) => {
+    return array.map((element) => element.substring(0, 1));
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(firstLettersArray(names));
+console.log(arrowFirstLettersArray(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
