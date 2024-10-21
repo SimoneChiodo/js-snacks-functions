dadf98534988867db1245d4805345929f76b7b77
SimoneChiodo <simochi.005@gmail.com>
2024-10-21 16:17:19 +0200

    esercizio 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,25 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function arrayStartLetter(array, letter) {
+    let result = [];
 
+    for (let i = 0; i < array.length; i++)
+        if (array[i].substring(0, 1) === letter.toUpperCase())
+            result.push(array[i]);
+
+    return result;
+}
+
+const arrowArrayStartLetter = (array, letter) => {
+    return array.filter(
+        (element) => element.substring(0, 1) === letter.toUpperCase()
+    );
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(arrayStartLetter(names, "a"));
+console.log(arrowArrayStartLetter(names, "a"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
