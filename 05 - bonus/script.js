14bc21bd03c739a5cd22ab0a21c1b7b278beb058
SimoneChiodo <simochi.005@gmail.com>
2024-10-21 16:50:41 +0200

    bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,33 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+function helloDay(name) {
+    const hour = new Date().getHours();
 
+    // Se è mattina
+    if (hour >= 6 && hour < 13) return `Buongiorno ${name}`;
+    // Se è pomeriggio
+    else if (hour >= 13 && hour < 17) return `Buon pomeriggio ${name}`;
+    // Se è sera
+    else return `Buonasera ${name}`;
+}
+
+const arrowHelloDay = (name) => {
+    const hour = new Date().getHours();
+
+    // Se è mattina
+    if (hour >= 6 && hour < 13) return `Buongiorno ${name}`;
+    // Se è pomeriggio
+    else if (hour >= 13 && hour < 17) return `Buon pomeriggio ${name}`;
+    // Se è sera
+    else return `Buonasera ${name}`;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(helloDay(name));
+console.log(arrowHelloDay(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
